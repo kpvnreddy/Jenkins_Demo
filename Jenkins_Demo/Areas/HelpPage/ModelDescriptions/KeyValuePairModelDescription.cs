cdeaa885ecd4238c4edcc00aa2473a9ec104b842
pavan reddy <kpvnreddy@outlook.com>
2017-01-31 12:28:39 +0530

    Add project files.

---
+++ Jenkins_Demo/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace Jenkins_Demo.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ Jenkins_Demo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Jenkins_Demo.WebApiApplication" Language="C#" %>
